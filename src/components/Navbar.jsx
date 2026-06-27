5724f5abb6452744a23c137ac9afdd2a6cf3897f
Avi <avinashshetty2004@gmail.com>
2026-06-27 22:47:58 +0530

    first commit of my portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Huzaif &nbsp;
-            <span className='sm:block hidden'> | Devxora</span>
+            Avinash &nbsp;
+            <span className='sm:block hidden'> | Shetty</span>
           </p>
         </Link>
 
